cd3b42fa187d94203e6c0fb2c580406b3ba91b7a
Christion Ross <christionross@uagateway.org>
2019-01-07 23:01:15 +0000

    tbc

--- js/script.js
+++ js/script.js
@@ -8,66 +8,47 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore = questionOne(q1Result) + questionTwo(q2Result) + questionThree(q3Result);
-        $(".result").text(answer(totalScore));
+        
+        
     });
         
-    function questionOne(q1Result){
-        if (q1Result === "Reading"){
-            return 1;
-        }
-        else if (q1Result === "Coding"){
-            return 2;
-        }
-        else if (q1Result === "Sports"){
+    function calculateQuestionOne(q1Result){
+        if (q1Result === "jumping"){
+         return 5;
+        }else{
             return 3;
         }
-        else if (q1Result === "Designing"){
+    }
+    
+    function calculateQuestionTwo(q2Result){
+     if (q2Result === "Fire"){
+         return 5;
+        }else if (q2Result == "Thunder"){
+            return 3;
+        }else if (q2Result == "Water"){
+        return 6;
+        }else{
+            return 1;
+        }
+    }
+    
+    function calculateQuestionThree(q3Result){
+ if (q3Result === "dog"){
+         return 5;
+        }else if (q3Result == "cat"){
+            return 3;
+        }else if (q3Result == "hedgehog"){
+            return 6;
+        }else if (q3Result == "bandicoot"){
             return 4;
+        }else if (q3Result == "monkey"){
+            return 7;
+        }else{
+            return 8;
         }
-    }
-    
-    function questionTwo(q2Result){
-        if (q2Result === "Pancakes"){
-            return 1;
-        }
-        else if (q2Result === "Waffles"){
-            return 2;
-        }
-        else if (q2Result === "French Toast"){
-            return 3;
-        }
-    }
-    
-    function questionThree(q3Result){
-        if (q3Result.length >= 3){
-            return 1;
-        }
-        else if (q3Result.length >= 6){
-            return 2;
-        }
-        else if (q3Result.length > 6) {
-            return 3;
-        }  
+        
         
     }
-    
-  function answer(totalScore){
-      if (totalScore <= 4){
-          return "Snapchat";
-          
-      }
-      
-     else if (totalScore >4 && totalScore <=  6){
-          return "Instagram";
-     }
-     
-     else if(totalScore> 6){
-        return "Facebook";
-     }
-          
-      
-  }
 
 
 });
